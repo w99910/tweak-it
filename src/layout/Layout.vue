97ff10bbedbdc6d9db78075e1ee161b73e7cfdda
Thomas <zawlintun@robust.best>
2024-12-11 14:58:24 +0700

    add alert

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -29,7 +29,7 @@ onMounted(() => {
         <div class="w-full py-3 gap-x-2 bg-primary text-dark flex items-center justify-center"> <svg
                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                class="lucide lucide-circle-alert">
+                class="lucide lucide-circle-alert w-6 h-6 min-w-4 min-h-4">
                 <circle cx="12" cy="12" r="10" />
                 <line x1="12" x2="12" y1="8" y2="12" />
                 <line x1="12" x2="12.01" y1="16" y2="16" />
